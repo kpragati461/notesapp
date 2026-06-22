f4081ba42da8ef4e667a056c5db9ac86b3f28b24
kpragati461 <katiyarpragati461@gmail.com>
2026-06-22 19:19:14 +0530

    Create NoteService

---
+++ src/main/java/com/don/notesapp/service/NoteService.java
@@ -0,0 +1,25 @@
+package com.don.notesapp.service;
+
+import com.don.notesapp.entity.Note;
+import com.don.notesapp.repository.NoteRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class NoteService {
+
+    private final NoteRepository noteRepository;
+
+    public NoteService(NoteRepository noteRepository) {
+        this.noteRepository = noteRepository;
+    }
+
+    public Note createNote(Note note) {
+        return noteRepository.save(note);
+    }
+
+    public List<Note> getAllNotes() {
+        return noteRepository.findAll();
+    }
+}
